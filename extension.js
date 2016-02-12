62ebf9c2ea6eef88c2ec45a2617ed322533bebea
Bryan Hoekstra <hooky@qrmaster.co.nz>
2016-02-13 11:34:23 +1300

    var -> let/const

--- extension.js
+++ extension.js
@@ -1,24 +1,24 @@
 "use strict";
-var vscode = require('vscode');
-var beautify = require('js-beautify');
-var path = require('path');
-var fs = require('fs');
+const vscode = require('vscode');
+const beautify = require('js-beautify');
+const path = require('path');
+const fs = require('fs');
 
 function findRecursive(dir, fileName) {
-	var fullPath = path.join(dir, fileName);
-	var nextDir = path.dirname(dir);
-	var result = fs.existsSync(fullPath) ? fullPath : null;
+	const fullPath = path.join(dir, fileName);
+	const nextDir = path.dirname(dir);
+	let result = fs.existsSync(fullPath) ? fullPath : null;
 	if (!result && (nextDir !== dir)) {
 		result = findRecursive(nextDir, fileName);
 	}
 	return result;
 }
-var dropComments = inText => inText.replace(/(\/\*.*\*\/)|\/\/.*(?:[\r\n]|$)/g, "");
+const dropComments = inText => inText.replace(/(\/\*.*\*\/)|\/\/.*(?:[\r\n]|$)/g, "");
 
 //register on activation
 function activate(context) {
-	var mergeOpts = function(opts, type) {
-		var finOpts = {};
+	const mergeOpts = function(opts, type) {
+		const finOpts = {};
 		for (let a in opts) {
 			if (a !== 'js' && a !== 'html' && a !== 'css') {
 				finOpts[a] = opts[a];
@@ -33,27 +33,27 @@ function activate(context) {
 		}
 		return finOpts;
 	};
-	var getBeautifyType = function(doc) {
+	const getBeautifyType = function(doc) {
 		return Promise.resolve()
 			.then(() => {
-				var type = doc.isUntitled ? "" : doc.fileName.split('.')
+				const type = doc.isUntitled ? "" : doc.fileName.split('.')
 					.pop()
 					.toLowerCase();
-				var cfg = vscode.workspace.getConfiguration('beautify');
+				const cfg = vscode.workspace.getConfiguration('beautify');
 				//if a type is set on the window, use that
 				//check if the file is in the users json schema set
-				var jsSchema = vscode.workspace.getConfiguration('json')
+				const jsSchema = vscode.workspace.getConfiguration('json')
 					.schemas;
 				if (jsSchema.length) {
-					var matcher = [];
-					var extMatch = n => ({
+					let matcher = [];
+					const extMatch = n => ({
 						pattern: n.startsWith("**/") ? n : ("**/" + n)
 					});
 					jsSchema.forEach(schema => {
 						if (typeof schema.fileMatch === 'string') {
 							matcher.push(extMatch(schema.fileMatch));
 						} else {
-							var t = schema.fileMatch.map(extMatch);
+							const t = schema.fileMatch.map(extMatch);
 							matcher = matcher.concat(t);
 						}
 					});
@@ -84,23 +84,23 @@ function activate(context) {
 				}
 			});
 	};
-	var doBeautify = function(active, doc, type, opts) {
-		var original = doc.getText();
+	const doBeautify = function(active, doc, type, opts) {
+		const original = doc.getText();
 		//get the whole file:
-		var range = new vscode.Range(new vscode.Position(0, 0), doc.positionAt(Infinity));
-		var result = beautify[type](original, opts);
+		const range = new vscode.Range(new vscode.Position(0, 0), doc.positionAt(Infinity));
+		const result = beautify[type](original, opts);
 		//and make the change:
 		active.edit(editor => editor.replace(range, result));
 	};
 	//it's ok to build and pass the re from outside of here, we always run
 	//to completion.
-	var disposable = vscode.commands.registerCommand('HookyQR.beautify', function() {
-		var active = vscode.window.activeTextEditor;
+	const disposable = vscode.commands.registerCommand('HookyQR.beautify', function() {
+		const active = vscode.window.activeTextEditor;
 		if (!active) return;
-		var doc = active.document;
+		const doc = active.document;
 		if (!doc) return;
 		//get a settings file
-		var base = vscode.workspace.rootPath;
+		let base = vscode.workspace.rootPath;
 
 		if (!doc.isUntitled) base = doc.fileName;
 
@@ -108,14 +108,14 @@ function activate(context) {
 		getBeautifyType(doc)
 			.then(type => {
 				if (!type) return; //user skipped the selection
-				var beautFile;
+				let beautFile;
 				if (base) beautFile = findRecursive(base, ".jsbeautifyrc");
 				//walk to find a .jsbeautifyrc
 				if (beautFile) fs.readFile(beautFile, function(ee, d) {
 					if (!d) d = "{}";
-					var opts = {};
+					let opts = {};
 					try {
-						var unCommented = dropComments(d.toString());
+						const unCommented = dropComments(d.toString());
 						opts = JSON.parse(unCommented);
 						opts = mergeOpts(opts, type);
 					} catch (e) {
